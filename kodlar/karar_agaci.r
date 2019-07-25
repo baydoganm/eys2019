1dd6911c91a44dda466b89a8876816238b767f8c
Mustafa Baydogan <baydoganmustafa@gmail.com>
2019-07-25 17:29:29 +0300

    Update karar_agaci.r

--- kodlar/karar_agaci.r
+++ kodlar/karar_agaci.r
@@ -2,7 +2,8 @@ library(rpart)
 library(rattle)
 library(data.table)
 
-veri_yolu="C:/mustafa/Seminer/EYS19/Data/Housing_Data/housingdata.csv"
+setwd('C:/mustafa/Seminer/EYS19/Data/Housing_Data/')
+veri_yolu="housingdata.csv"
 ev_veri=fread(veri_yolu)
 sutun_isimler=fread('names.txt',header=F)
 names(ev_veri)=sutun_isimler$V1
